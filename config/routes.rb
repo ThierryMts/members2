f596a938b0f002f0eb1dbad2939a43a45fc8dcdb
SphinxToSETH <thierrymateus@hotmail.fr>
2017-10-31 18:01:57 +0100

    members2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  get 'users/new'
+  
+  get 'static_pages/home'
+
+  get 'static_pages/pagesecrete'
+
+root 'static_pages#home'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  post '/users',  to: 'users#create'
+
+
+resources :users
+
 end
